eeab8ca66faa84deb646a69c85bf2438eb76dca1
Héctor Moreno <hmoreno@nearsoft.com>
2016-10-15 12:56:10 -0700

    Create MovieCell custom

---
+++ MyFavMovies/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MyFavMovies
+//
+//  Created by Héctor Moreno on 15/10/16.
+//  Copyright © 2016 Héctor Moreno. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
