e3edd56ca36f2683ead33e7d7bd0c1539db29e14
Héctor Moreno <hmoreno@nearsoft.com>
2016-10-15 13:45:50 -0700

    Add func saveMovies, loadMovies, addMovies and loadedMovies array

--- MyFavMovies/DataService.swift
+++ MyFavMovies/DataService.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import UIKit
+
+class DataService {
+    
+    static let instanse = DataService()
+    private var _loadedMovies = [Movie]()
+    
+    var loadedMovies: [Movie]{
+        return _loadedMovies
+    }
+    
+    func saveMovies() {
+    
+    }
+
+    func loadMovies() {
+    
+    }
+
+    func addMovies(movie: Movie){
+        self._loadedMovies.append(movie)
+        saveMovies()
+        loadMovies()
+    
+    }
+
+}
