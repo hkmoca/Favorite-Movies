64f2bc25ab01061ffa258fde2a30c0fb94601be5
Héctor Moreno <hmoreno@nearsoft.com>
2016-10-15 15:22:24 -0700

    Create and join var to TextFields on main storyboards.

--- MyFavMovies/AddMovieVC.swift
+++ MyFavMovies/AddMovieVC.swift
@@ -8,28 +8,46 @@
 
 import UIKit
 
-class AddMovieVC: UIViewController {
+class AddMovieVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var descField: UITextField!
+    @IBOutlet weak var linkField: UITextField!
+    
+    var imagePicker: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        movieImage.layer.cornerRadius = movieImage.frame.size.width / 2
+        movieImage.clipsToBounds = true
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func createBtnPressed(sender: AnyObject){
+        if let title = titleField.text, let desc = descField.text, let link = linkField.text, let image = movieImage.image {
+        
+            let imagePath = DataService.instanse.saveImageAndCreatePath(image)
+            
+            let movie = Movie(title: title, movieDescription: desc, imagePath: imagePath, link: link)
+            DataService.instanse.addMovies(movie)
+            self.navigationController?.popViewControllerAnimated(true)
+        }
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func addPicBtnPressed(sender: UIButton){
+        sender.setTitle("", forState: .Normal)
+        presentViewController(imagePicker, animated: true, completion: nil)
+    
     }
-    */
-
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        movieImage.image = image
+    }
+    
+    
 }
